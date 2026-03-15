1fed0f4a30d587b2bc2c662322b673e64527fa7a
RamanKarpenka <s34457@pjwstk.edu.pl>
2026-03-15 16:03:04 +0100

    add ReadLine after quetion

--- Program.cs
+++ Program.cs
@@ -4,5 +4,7 @@
     {
         Console.WriteLine("Application to math methods");
         Console.WriteLine("What is your name?: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hi " +  name);
     }
 }
